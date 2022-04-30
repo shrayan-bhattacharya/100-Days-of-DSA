777dcf6fc59181f0686f89fe3b236fd8716ef7df
Shrayan Bhattacharya <shrayanjob08@gmail.com>
2022-04-30 06:50:22 +0530

    Day 5 Completed

---
+++ Day 5/TwoSum.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/two-sum/
+
+public class TwoSum {
+
+  public static int[] twoSum(int[] nums, int target) {
+    int[] result = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if ((nums[i] + nums[j]) == target) {
+          result[0] = i;
+          result[1] = j;
+        }
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int nums[] = { 2, 7, 11, 15 };
+    int target = 9;
+    int arr[] = new int[2];
+    arr = twoSum(nums, target);
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]);
+    }
+  }
+}
